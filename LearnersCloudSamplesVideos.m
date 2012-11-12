f9e02f7bc1e63d5b187274ceab6b571b695c8778
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-11-12 16:04:39 +0000

    minor

--- LearnersCloudSamplesVideos.m
+++ LearnersCloudSamplesVideos.m
@@ -97,7 +97,7 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
 	
-	[self willAnimateRotationToInterfaceOrientation:self.interfaceOrientation duration:1];
+	
 	
 }
 
@@ -214,7 +214,7 @@
     
 	
     
-	
+	[self willAnimateRotationToInterfaceOrientation:self.interfaceOrientation duration:1];
 	
 	return cell;
 	
